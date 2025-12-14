f74d5071d5ae4db04f0a8c298172cbe007c1a538
benisamuel <benisamuel566@gmail.com>
2025-12-14 21:30:54 +0200

    added framer

--- src/components/Service/ServiceWrapper.tsx
+++ src/components/Service/ServiceWrapper.tsx
@@ -1,23 +1,43 @@
-import React from 'react'
-import ServiceCard from './ServiceCard';
-import serviceTypes from '../../types/Service/ServiceTypes';
+import React from "react";
+import { motion } from "framer-motion";
+import ServiceCard from "./ServiceCard";
+import serviceTypes from "../../types/Service/ServiceTypes";
 
 interface ServiceWrapperProps {
-    services: serviceTypes[];
+  services: serviceTypes[];
 }
 
 const ServiceWrapper: React.FC<ServiceWrapperProps> = ({ services }) => {
-  return (
-    <div className=' flex flex-col md:flex-row gap-4 '>
-        {
-            services.map((service) => {
-                return (
-                    <ServiceCard key={service.id} image={service.image} category={service.category} description={service.description}/>
-                )
-            })
-        }
-    </div>
-  )
-}
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.15,
+      },
+    },
+  };
 
-export default ServiceWrapper
+  return (
+    <motion.div
+      className="flex flex-col md:flex-row gap-6"
+      variants={containerVariants}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, margin: "-100px" }}
+    >
+      {services.map((service) => {
+        return (
+          <ServiceCard
+            key={service.id}
+            image={service.image}
+            category={service.category}
+            description={service.description}
+          />
+        );
+      })}
+    </motion.div>
+  );
+};
+
+export default ServiceWrapper;
