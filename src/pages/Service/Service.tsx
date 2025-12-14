4443d872298fb9b4fed631521706da1d585724b7
benisamuel <benisamuel566@gmail.com>
2025-12-14 21:34:16 +0200

    added framer motions

--- src/pages/Service/Service.tsx
+++ src/pages/Service/Service.tsx
@@ -1,47 +1,69 @@
-import React from 'react'
-import DashMain from '../../components/Dashboard/DashMain'
-import menu from "../../assets/Dashboard/menu.svg"
-import settings from "../../assets/Dashboard/settings.svg"
-import whiteMode from "../../assets/Dashboard/whiteMode.svg"
-import ServiceWrapper from '../../components/Service/ServiceWrapper'
+import React from "react";
+import { motion } from "framer-motion";
+import DashMain from "../../components/Dashboard/DashMain";
+import ThemeControls from "../../components/Theme/ThemeControls";
+import { useThemeColors } from "../../hooks/useThemeColors";
+import menu from "../../assets/Dashboard/menu.svg";
+import ServiceWrapper from "../../components/Service/ServiceWrapper";
 import services from "../../Data/ServiceData";
-import services2 from '../../Data/ServiceData2'
-
+import services2 from "../../Data/ServiceData2";
 
 const Service: React.FC = () => {
-    return (
-        <div>
-            <DashMain>
-                <div className=' py-16 pl-10 relative' >
-                    <div className=" absolute top-9 left-10 cursor-pointer md:hidden">
-                        <img src={menu} />
-                    </div>
-                    <div className=" absolute top-9 right-10 flex flex-col gap-4">
-                        <div>
-                            <img src={settings} className=" h-4 w-4 cursor-pointer" />
-                        </div>
-                        <div>
-                            <img src={whiteMode} className=" h-4 w-4 cursor-pointer" />
-                        </div>
-                    </div>
+  const { themeColor } = useThemeColors();
 
-                    <div className=' flex flex-col gap-2 py-4 md:py-0 '>
-                        <p className=' font-poppins text-sm text-white font-semibold selection:bg-[#ED2929] '>Services</p>
-                        <div className=' flex flex-col gap-1'>
-                            <div className=' bg-[#ED2929] w-16 h-1 rounded-lg' />
-                            <div className=' bg-[#ED2929] w-10 h-1 rounded-lg' />
-                        </div>
-                    </div>
+  return (
+    <div className="relative overflow-hidden">
+      <DashMain>
+        <div className="py-16 pl-10 relative">
+          <motion.div
+            className="absolute top-9 left-10 cursor-pointer md:hidden z-20"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <img src={menu} alt="menu" />
+          </motion.div>
+          <ThemeControls />
 
-                    <div className=' flex flex-col py-8 gap-12 pr-10 '>
-                        <ServiceWrapper services={services} />
-                        <ServiceWrapper services={services2}/>
-                    </div>
+          <motion.div
+            className="flex flex-col gap-2 py-4 md:py-0"
+            initial={{ opacity: 0, x: -30 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+          >
+            <motion.p
+              className="font-poppins text-2xl md:text-3xl text-white font-semibold"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.2 }}
+            >
+              Services
+            </motion.p>
+            <div className="flex flex-col gap-1">
+              <motion.div
+                className="w-16 h-1 rounded-lg"
+                style={{ backgroundColor: themeColor }}
+                initial={{ width: 0 }}
+                animate={{ width: 64 }}
+                transition={{ delay: 0.4, duration: 0.5 }}
+              />
+              <motion.div
+                className="w-10 h-1 rounded-lg"
+                style={{ backgroundColor: themeColor }}
+                initial={{ width: 0 }}
+                animate={{ width: 40 }}
+                transition={{ delay: 0.5, duration: 0.5 }}
+              />
+            </div>
+          </motion.div>
 
-                </div>
-            </DashMain>
+          <div className="flex flex-col py-8 gap-12 pr-10">
+            <ServiceWrapper services={services} />
+            <ServiceWrapper services={services2} />
+          </div>
         </div>
-    )
-}
+      </DashMain>
+    </div>
+  );
+};
 
-export default Service
+export default Service;
