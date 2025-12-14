e0e7b5b04ebce672b63d870a0347330981d56d43
benisamuel <benisamuel566@gmail.com>
2025-12-14 21:35:28 +0200

    improved overall layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,122 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import Service from './pages/Service/Service'
-import Projects from './pages/Projects/Projects'
-import Contact from './pages/Contact/Contact'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
+import { ThemeProvider } from './context/ThemeContext';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Service from './pages/Service/Service';
+import Projects from './pages/Projects/Projects';
+import Contact from './pages/Contact/Contact';
 
-const App: React.FC  = () => {
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    y: 20,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      ease: 'easeOut',
+    },
+  },
+  exit: {
+    opacity: 0,
+    y: -20,
+    transition: {
+      duration: 0.3,
+      ease: 'easeIn',
+    },
+  },
+};
+
+const AnimatedRoutes: React.FC = () => {
+  const location = useLocation();
+
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />}/>
-          <Route path='/service' element={<Service />}/>
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
-    </div>
-  )
-}
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <motion.div
+              variants={pageVariants}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+            >
+              <Home />
+            </motion.div>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <motion.div
+              variants={pageVariants}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+            >
+              <About />
+            </motion.div>
+          }
+        />
+        <Route
+          path="/service"
+          element={
+            <motion.div
+              variants={pageVariants}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+            >
+              <Service />
+            </motion.div>
+          }
+        />
+        <Route
+          path="/projects"
+          element={
+            <motion.div
+              variants={pageVariants}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+            >
+              <Projects />
+            </motion.div>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <motion.div
+              variants={pageVariants}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+            >
+              <Contact />
+            </motion.div>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
-export default App
+const App: React.FC = () => {
+  return (
+    <ThemeProvider>
+      <div className="min-h-screen bg-[#020202] dark-mode">
+        <Router>
+          <AnimatedRoutes />
+        </Router>
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default App;
