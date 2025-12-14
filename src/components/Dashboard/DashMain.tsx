e330dc52e7cb63cb9bda059586b3f8bfd4fba43f
benisamuel <benisamuel566@gmail.com>
2025-12-14 21:29:17 +0200

    improved dashboadMain layout

--- src/components/Dashboard/DashMain.tsx
+++ src/components/Dashboard/DashMain.tsx
@@ -1,18 +1,19 @@
-import React, { ReactNode } from 'react'
-import DashLeft from './DashLeft'
-import DashRight from './DashRight'
+/* eslint-disable react/no-children-prop */
+import React, { ReactNode } from "react";
+import DashLeft from "./DashLeft";
+import DashRight from "./DashRight";
 
 type DashMainProps = {
-    children: ReactNode;
-}
+  children: ReactNode;
+};
 
-const DashMain: React.FC<DashMainProps> = ({children}) => {
+const DashMain: React.FC<DashMainProps> = ({ children }) => {
   return (
-    <div className=' flex flex-row '>
-        <DashLeft />
-        <DashRight children={children} />
+    <div className=" flex flex-row ">
+      <DashLeft />
+      <DashRight children={children} />
     </div>
-  )
-}
+  );
+};
 
-export default DashMain
+export default DashMain;
