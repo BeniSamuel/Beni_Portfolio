976e5cecfd5c6beced30413565af30469c773cb2
benisamuel <benisamuel566@gmail.com>
2025-12-14 21:30:05 +0200

    fixed eslint issue

--- src/components/Home/DynamicText.tsx
+++ src/components/Home/DynamicText.tsx
@@ -1,29 +1,32 @@
-import React from 'react'
-import { Typewriter } from 'react-simple-typewriter';
+import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+import { useThemeColors } from "../../hooks/useThemeColors";
 
 const DynamicText: React.FC = () => {
-  return (
-    <div>
-        <Typewriter 
-            words={[
-                "Backend Developer",
-                "Frontend Developer",
-                "Mobile Developer",
-                "Desktop App Developer",
-                "Into Cybersecurity",
-                "Embedded Designer"  
-            ]}
-            loop = {Infinity}
-            cursor
-            cursorColor='#ED2929'
-            cursorStyle="|"
-            cursorBlinking={false}
-            typeSpeed={150}
-            deleteSpeed={150}
-            delaySpeed={1000}
-        />
-    </div>
-  )
-}
+  const { themeColor } = useThemeColors();
 
-export default DynamicText
+  return (
+    <span>
+      <Typewriter
+        words={[
+          "Backend Developer",
+          "Frontend Developer",
+          "Mobile Developer",
+          "Desktop App Developer",
+          "Into Cybersecurity",
+          "Embedded Designer",
+        ]}
+        loop={Infinity}
+        cursor
+        cursorColor={themeColor}
+        cursorStyle="|"
+        cursorBlinking={false}
+        typeSpeed={150}
+        deleteSpeed={150}
+        delaySpeed={1000}
+      />
+    </span>
+  );
+};
+
+export default DynamicText;
